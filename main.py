304ad412cd58107b80fe76bee07d6fd7e12c521a
Botir Akhmedov <95535127+botirpyjs@users.noreply.github.com>
2021-12-09 21:23:51 +0300

    bot

---
+++ main.py
@@ -0,0 +1,20 @@
+from transliterate import to_cyrillic, to_latin
+import telebot
+
+token = '5061158248:AAHHz-5OKdOfBCAgjbb2t7_b_SgPxLeTW2Q'
+bot = telebot.TeleBot(token, parse_mode=None)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    javob = "Assalomu alaykum mening Pythonda ishlangan botimga hush kelibsiz!"
+    javob += "\nBu botning vazifasi Krillcha so'zlarni Lotin alifbosidagi so'zlarga alishtiradi.\nAksincha Lotin alifbosidagi so'zlarni Krill so'zlariga alishtirib beradi."
+    javob1 = "\n\nMatn kiriting:"
+    bot.reply_to(message, javob + javob1)
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    msg = message.text
+    javob = lambda msg: to_cyrillic(msg) if msg.isascii() else to_latin(msg)
+    
+    bot.reply_to(message, javob(msg))            
+bot.polling()
